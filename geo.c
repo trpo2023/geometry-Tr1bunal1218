1ee3bbd90c60edca798507f6473f8acb0c5dd8f3
Dimitry Detsyuk <dimitry05@users.noreply.github.com>
2023-04-08 13:08:59 +0700

    Formating geo.c

--- geo.c
+++ geo.c
@@ -1,14 +1,13 @@
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAX_LEN 100
 
-int main() {
+int main()
+{
     char input[MAX_LEN];
     printf("Enter the circle parameters in format circle(x y,r):");
-  fgets(input, MAX_LEN, stdin);
-
-
+    fgets(input, MAX_LEN, stdin);
 
     float x, y, r;
     if (strncmp(input, "circle(", 7) != 0) {
@@ -17,9 +16,10 @@ int main() {
     }
 
     if (sscanf(input, "circle(%f %f,%f)", &x, &y, &r) != 3) {
-        printf("\nError: incorrect input format. Please enter the circle parameters in format circle(x y,r).\n");
+        printf("\nError: incorrect input format. Please enter the circle "
+               "parameters in format circle(x y,r).\n");
         return 0;
     }
-    printf("circle(%.2f %.2f,%.2f)", x,y,r);
+    printf("circle(%.2f %.2f,%.2f)", x, y, r);
     return 0;
 }
